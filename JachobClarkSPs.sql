9956b5962bf71fcdf6b1ec16c5f2b6ebe97098cf
jc00042 <142524965+jc00042@users.noreply.github.com>
2024-02-27 21:02:40 -0500

    Updated and Completed JachobClarkSPs.sql

--- JachobClarkSPs.sql
+++ JachobClarkSPs.sql
@@ -1,1 +1,32 @@
+use WeatherAppDB
+go
 
+/**  Shows a player, the team they play for, and their position **/
+CREATE PROC spPlayerInfo
+@PID INT
+AS
+BEGIN
+SELECT P.PName, P.PPosition, T.TeamName 
+FROM NewPlayer as P
+INNER JOIN Team as T on P.TeamID = T.TeamID
+WHERE PID = @PID;
+
+END;
+GO
+
+EXEC spPlayerInfo @PID = 1;
+GO
+
+/** Shows how many teams are in one Location**/
+CREATE PROC spSameLocation
+@TeamLocation VARCHAR(50)
+AS
+BEGIN
+SELECT T.TeamID, T.TeamName
+FROM TEAM as T
+WHERE TeamLocation = @TeamLocation
+END;
+GO
+
+EXECUTE spSameLocation @TeamLocation = 'London';
+GO
