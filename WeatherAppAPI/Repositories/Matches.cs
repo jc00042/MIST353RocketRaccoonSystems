956eb10049edd75f5739f086916a2ab8f8d7fdc5
agf00006 <142524808+agf00006@users.noreply.github.com>
2024-03-28 15:41:52 -0400

    Update Matches.cs

--- WeatherAppAPI/Repositories/Matches.cs
+++ WeatherAppAPI/Repositories/Matches.cs
@@ -4,19 +4,19 @@ using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 // This API was done by Andrew Forman
 
-namespace APItest.Repositories
+namespace WeatherAppAPI.Repositories
 {
     public class Matches : IMatches
     {
-        private readonly DBContext _dbContext;
-        public Matches(DBContext dbContext) 
+        private readonly DbContextClass _dbContextClass;
+        public Matches(DbContextClass dbContextClass) 
         {
-            _dbContext = dbContext;
+            _dbContextClass = dbContextClass;
         }
         public async Task<List<MatchResults>> GetMatchResults(int MatchResultsID)
         {
             var param = new SqlParameter("@MatchResultsID", MatchResultsID);
-            var matchDetails = await Task.Run(() => _dbContext.MatchResults.FromSqlRaw("exec spGetMatchResults @MatchResultsID", param).ToListAsync());
+            var matchDetails = await Task.Run(() => _dbContextClass.MatchResults.FromSqlRaw("exec spGetMatchResults @MatchResultsID", param).ToListAsync());
             return matchDetails;
         }
     }
