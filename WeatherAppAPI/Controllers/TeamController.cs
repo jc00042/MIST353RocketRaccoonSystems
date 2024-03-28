27f0dbfce7fd0555a2c0598f1c8d52ac8e7a4e9d
jc00042 <142524965+jc00042@users.noreply.github.com>
2024-03-27 23:49:08 -0400

    Update TeamController.cs

--- WeatherAppAPI/Controllers/TeamController.cs
+++ WeatherAppAPI/Controllers/TeamController.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using WeatherAppAPI.Entities;
 using WeatherAppAPI.Repositories;
 
 //This API done by Jachob Clark
+/**
+ * This controller is the controller for ITeamService.
+ * ITeamService is a service that will get you the list
+ * of teams in the same desired city. The naming convention
+ * does not say what it does directly and can be misleading.
+ */
 
 namespace WeatherAppAPI.Controllers
 {
