bebc716f50bd582aede141118a2994d120a0af64
agf00006 <142524808+agf00006@users.noreply.github.com>
2024-03-28 15:35:19 -0400

    Create Matches.cs

---
+++ WeatherAppAPI/Repositories/Matches.cs
@@ -0,0 +1,24 @@
+using APItest.Data;
+using APItest.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+// This API was done by Andrew Forman
+
+namespace APItest.Repositories
+{
+    public class Matches : IMatches
+    {
+        private readonly DBContext _dbContext;
+        public Matches(DBContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<List<MatchResults>> GetMatchResults(int MatchResultsID)
+        {
+            var param = new SqlParameter("@MatchResultsID", MatchResultsID);
+            var matchDetails = await Task.Run(() => _dbContext.MatchResults.FromSqlRaw("exec spGetMatchResults @MatchResultsID", param).ToListAsync());
+            return matchDetails;
+        }
+    }
+}
