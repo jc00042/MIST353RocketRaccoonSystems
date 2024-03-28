63c9dfe65440d7fe06dd119b5eb209fb375d0773
jc00042 <142524965+jc00042@users.noreply.github.com>
2024-03-27 23:47:21 -0400

    Update ITeamService.cs

--- WeatherAppAPI/Repositories/ITeamService.cs
+++ WeatherAppAPI/Repositories/ITeamService.cs
@@ -1,6 +1,12 @@
-﻿using WeatherAppAPI.Entities;
+using WeatherAppAPI.Entities;
 
 //This API done by Jachob Clark
+/**
+ * This interface is the interface for ITeamService.
+ * ITeamService is a service that will get you the list
+ * of teams in the same desired city. The naming convention
+ * does not say what it does directly and can be misleading.
+ */
 
 namespace WeatherAppAPI.Repositories
 {
