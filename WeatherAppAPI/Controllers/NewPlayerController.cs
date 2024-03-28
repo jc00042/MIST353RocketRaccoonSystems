bd5e1b9de8706c9658ddeaf8f09c42be7db4c7a1
jc00042 <142524965+jc00042@users.noreply.github.com>
2024-03-27 23:49:33 -0400

    Update NewPlayerController.cs

--- WeatherAppAPI/Controllers/NewPlayerController.cs
+++ WeatherAppAPI/Controllers/NewPlayerController.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using WeatherAppAPI.Entities;
 using WeatherAppAPI.Repositories;
 
 //This API done by Jachob Clark
+/**
+ * This controller is the controller for NewPlayerService.
+ * NewPlayerService is a service that will get you the information
+ * of a desired player. The naming convention does not say what it
+ * does directly and can be misleading.
+ */
 
 namespace WeatherAppAPI.Controllers
 {
