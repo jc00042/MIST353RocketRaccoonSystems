184654e0842631cd00c1d46e372d0a0aaeb144ad
jc00042 <142524965+jc00042@users.noreply.github.com>
2024-03-27 23:45:13 -0400

    Update INewPlayerService.cs

--- WeatherAppAPI/Repositories/INewPlayerService.cs
+++ WeatherAppAPI/Repositories/INewPlayerService.cs
@@ -1,6 +1,13 @@
-﻿using WeatherAppAPI.Entities;
+using WeatherAppAPI.Entities;
 
 //This API done by Jachob Clark
+
+/**
+ * This interface is the interface for NewPlayerService.
+ * NewPlayerService is a service that will get you the information
+ * of a desired player. The naming convention does not say what it
+ * does directly and can be misleading.
+ */
 
 namespace WeatherAppAPI.Repositories
 {
